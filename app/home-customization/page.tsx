661d3cff598a50cd091d13d6951afe30dd61edd1
Santiago11234 <89491348+Santiago11234@users.noreply.github.com>
2024-01-17 17:00:56 -0600

    solar panel animation works

--- app/home-customization/page.tsx
+++ app/home-customization/page.tsx
@@ -7,11 +7,15 @@ import HomeContainer from "@/components/home-customization/home-container";
 export default function Home() {
   const [showSolarPanel, setShowSolarPanel] = useState(false);
 
+  const handleShowSolarPanel  = () =>  {
+    setShowSolarPanel(true);
+  }
+
   return (
     <div className="mt-11 flex min-h-screen w-full flex-col items-center gap-y-12">
       <div className="grid min-h-screen w-full grid-cols-6 grid-rows-1 gap-3 text-white px-4 py-4">
         <HomeContainer showSolarPanel={showSolarPanel} />
-        <CustomizationContainer setShowSolarPanel={setShowSolarPanel} />
+        <CustomizationContainer handleShowSolarPanel={handleShowSolarPanel} />
       </div>
     </div>
   );
--- components/home-customization/customization-container.tsx
+++ components/home-customization/customization-container.tsx
@@ -3,23 +3,38 @@
 import React from "react";
 
 type Props = {
-  setShowSolarPanel: (show: boolean) => void;
+  // setShowSolarPanel: (show: boolean) => void;
+  handleShowSolarPanel: () => void;
 };
 
 
 
-const CustomizationContainer: React.FC<Props> = ({ setShowSolarPanel }) => {
+const CustomizationContainer: React.FC<Props> = ({ handleShowSolarPanel }) => {
   const clicked = () => {
     console.log('clicked');
-    setShowSolarPanel(true);
+    handleShowSolarPanel();
   }
+
+
+    //   <button
+  //   className="p-2 bg-blue-500 text-white rounded z-50"
+  //   onClick={() => clicked()}
+  // >
+  //   Add Solar Panel
+  // </button>
+
   return (
-    <button
-    className="p-2 bg-blue-500 text-white rounded z-50"
-    onClick={() => clicked()}
-  >
-    Add Solar Panel
-  </button>
+
+    <div className="card col-span-1 ">
+      <div className="flex flex-col justify-center items-center h-full">
+        <button
+          className="p-2 bg-blue-500 text-white rounded z-50"
+          onClick={() => clicked()}
+        >
+          Add Solar Panel
+        </button>
+      </div>
+    </div>
   );
 };
 
